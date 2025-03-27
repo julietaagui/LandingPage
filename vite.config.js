cc9661c85ba2df17ed18f7e056931107772748fd
julietaagui <aguilerajulieta700@gmail.com>
2025-03-27 13:47:45 -0300

    resolviendo conflictos

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Asegura rutas relativas si lo subes a una subcarpeta
+  base: './', 
 });
